ac694391291a412cbf99e77a6f754a3e9ac20166
coquito77 <moises.e01@gmail.com>
2017-05-10 16:04:19 -0700

    Update abbFileName.R

--- abbFileName.R
+++ abbFileName.R
@@ -1,25 +1,25 @@
 abbFileName <- function(x) {
-
-FileName <- x %>% 
-  tolower %>% gsub("\\b(\\w)",  "\\U\\1",  ., perl=TRUE)
-
-FileNameList <-  gsub("[^[:alnum:]]", " ", FileName) %>% strsplit(" ") %>% unlist()
-
-for (i in 1:length(FileNameList)) {
   
-  #print(FileNameList[i])
+  FileName <- x %>% gsub("[^[:alnum:]]", " ", .) %>%
+    tolower %>% gsub("\\b(\\w)",  "\\U\\1",  ., perl=TRUE)
   
-  newval <- abbreviate(FileNameList[i])
+  FileNameList <- FileName %>% strsplit(" ") %>% unlist()
   
-  #print(newval)
+  for (i in 1:length(FileNameList)) {
+    
+    #print(FileNameList[i])
+    
+    newval <- abbreviate(FileNameList[i])
+    
+    #print(newval)
+    
+    x <- sub(FileNameList[i], newval, FileName)
+    
+    FileName <-  gsub("[^[:alnum:]]", "", x)
+    
+  }
   
-  x <- sub(FileNameList[i], newval, FileName)
-  
-  FileName <-  gsub("[^[:alnum:]]", "", x)
-  
-}
-
   print(FileName)
 }
 
- # e.g abbFileName("Data Show Differences Between Urban and Rural Populations")
+# e.g abbFileName("Data Show Differences Between Urban and Rural Populations")
