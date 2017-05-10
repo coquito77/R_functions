b3559e1f2d723f19ae175e7db89e22293e229b75
coquito77 <moises.e01@gmail.com>
2017-05-10 10:43:38 -0700

    Function to abbreviate strings

---
+++ abbFileName.R
@@ -0,0 +1,29 @@
+## fuction to abbreviate stings e.g. file names of column names
+## it needs the package library(dplyr)
+
+
+abbFileName <- function(x) {
+
+FileName <- x %>% 
+  tolower %>% gsub("\\b(\\w)",  "\\U\\1",  ., perl=TRUE)
+
+FileNameList <-  gsub("[^[:alnum:]]", " ", FileName) %>% strsplit(" ") %>% unlist()
+
+for (i in 1:length(FileNameList)) {
+  
+  #print(FileNameList[i])
+  
+  newval <- abbreviate(FileNameList[i])
+  
+  #print(newval)
+  
+  x <- sub(FileNameList[i], newval, FileName)
+  
+  FileName <-  gsub("[^[:alnum:]]", "", x)
+  
+}
+
+  print(FileName)
+}
+
+abbFileName("validation GROW Job Placement Report ")
